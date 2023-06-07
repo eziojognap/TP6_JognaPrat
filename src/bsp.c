e5700332a61457796b29de52a07ea24b571e7e8d
eziojognap <eziojognap@gmail.com>
2023-06-07 10:34:02 -0300

    Mas modificaciones para usar poncho en bsp.c

--- src/bsp.c
+++ src/bsp.c
@@ -6,70 +6,12 @@
 /************************************* INCLUDES ***************************************************/
 #include "bsp.h"
 #include "chip.h"
+#include "poncho.h"
+#include "digital.h"
 
 /************************************* EXTERN VARIABLES *******************************************/
 
 /************************************* PRIVATE MACROS AND DEFINES *********************************/
-
-#define LED_R_PORT 2
-#define LED_R_PIN  0
-#define LED_R_FUNC SCU_MODE_FUNC4
-#define LED_R_GPIO 5
-#define LED_R_BIT  0
-
-#define LED_G_PORT 2
-#define LED_G_PIN  1
-#define LED_G_FUNC SCU_MODE_FUNC4
-#define LED_G_GPIO 5
-#define LED_G_BIT  1
-
-#define LED_B_PORT 2
-#define LED_B_PIN  2
-#define LED_B_FUNC SCU_MODE_FUNC4
-#define LED_B_GPIO 5
-#define LED_B_BIT  2
-
-#define LED_1_PORT 2
-#define LED_1_PIN  10
-#define LED_1_FUNC SCU_MODE_FUNC0
-#define LED_1_GPIO 0
-#define LED_1_BIT  14
-
-#define LED_2_PORT 2
-#define LED_2_PIN  11
-#define LED_2_FUNC SCU_MODE_FUNC0
-#define LED_2_GPIO 1
-#define LED_2_BIT  11
-
-#define LED_3_PORT 2
-#define LED_3_PIN  12
-#define LED_3_FUNC SCU_MODE_FUNC0
-#define LED_3_GPIO 1
-#define LED_3_BIT  12
-
-#define TEC_1_PORT 1
-#define TEC_1_PIN  0
-#define TEC_1_FUNC SCU_MODE_FUNC0
-#define TEC_1_GPIO 0
-#define TEC_1_BIT  4
-
-#define TEC_2_PORT 1
-#define TEC_2_PIN  1
-#define TEC_2_FUNC SCU_MODE_FUNC0
-#define TEC_2_GPIO 0
-#define TEC_2_BIT  8
-
-#define TEC_3_PORT 1
-#define TEC_3_PIN  2
-#define TEC_3_FUNC SCU_MODE_FUNC0
-#define TEC_3_GPIO 0
-#define TEC_3_BIT  9
-
-#define TEC_4_PORT 1
-#define TEC_4_PIN  6
-#define TEC_4_FUNC SCU_MODE_FUNC0
-#define TEC_4_GPIO 1
-#define TEC_4_BIT  9
 
 /************************************* PRIVATE TYPEDEFS *******************************************/
 static struct board_estructura board = {0};
@@ -79,45 +21,164 @@ static struct board_estructura board = {0};
 /************************************* GLOBAL VARIABLES *******************************************/
 
 /************************************* STATIC FUNCTION PROTOTYPES *********************************/
+void DigitsInit (void);
+
+void SegmentsInit (void);
+
+void BuzzerInit (void);
+
+void KeysInit (void);
+
+void ScreenTurnOff (void);
+
+void SegmnetsTurnOn (uint8_t segments);
+
+void DigitTurnOn (uint8_t digit);
 
 /************************************* STATIC FUNCTIONS *******************************************/
 
 /************************************* GLOBAL FUNCTIONS *******************************************/
+// board_puntero BoardCreate(void) {
+//     /********* LED RGB **********/
+//     Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
+//     Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, false);
+//     Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, true);
+
+//     Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
+//     Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, false);
+//     Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, true);
+
+//     Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
+//     board.led_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
+
+//     /********** LEDs 1 2 y 3 ********/
+//     Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
+//     board.led_rojo = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
+
+//     Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
+//     board.led_amarillo = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
+
+//     Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
+//     board.led_verde = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
+
+//     /********* Teclas 1 2 3 y 4 *********/
+//     Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
+//     board.Boton1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
+
+//     Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
+//     board.Boton2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
+
+//     Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
+//     board.Boton3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
+
+//     Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
+//     board.Boton4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+
+//     return &board;
+// }
+
+void DigitsInit(void){
+    Chip_SCU_PinMuxSet (DIGIT_1_PORT, DIGIT_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | DIGIT_1_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, DIGIT_1_GPIO, DIGIT_1_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, DIGIT_1_GPIO, DIGIT_1_BIT, true);
+
+    Chip_SCU_PinMuxSet (DIGIT_2_PORT, DIGIT_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | DIGIT_2_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, DIGIT_2_GPIO, DIGIT_2_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, DIGIT_2_GPIO, DIGIT_2_BIT, true);
+
+    Chip_SCU_PinMuxSet (DIGIT_3_PORT, DIGIT_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | DIGIT_3_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, DIGIT_3_GPIO, DIGIT_3_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, DIGIT_3_GPIO, DIGIT_3_BIT, true);
+
+    Chip_SCU_PinMuxSet (DIGIT_4_PORT, DIGIT_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | DIGIT_4_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, DIGIT_4_GPIO, DIGIT_4_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, DIGIT_4_GPIO, DIGIT_4_BIT, true);
+}
+
+void SegmentsInit(void){
+    Chip_SCU_PinMuxSet (SEGMENT_A_PORT, SEGMENT_A_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_A_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_A_GPIO, SEGMENT_A_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_A_GPIO, SEGMENT_A_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_B_PORT, SEGMENT_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_B_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_B_GPIO, SEGMENT_B_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_B_GPIO, SEGMENT_B_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_C_PORT, SEGMENT_C_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_C_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_C_GPIO, SEGMENT_C_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_C_GPIO, SEGMENT_C_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_D_PORT, SEGMENT_D_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_D_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_D_GPIO, SEGMENT_D_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_D_GPIO, SEGMENT_D_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_E_PORT, SEGMENT_E_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_E_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_E_GPIO, SEGMENT_E_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_E_GPIO, SEGMENT_E_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_F_PORT, SEGMENT_F_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_F_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_F_GPIO, SEGMENT_F_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_F_GPIO, SEGMENT_F_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_G_PORT, SEGMENT_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_G_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_G_GPIO, SEGMENT_G_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_G_GPIO, SEGMENT_G_BIT, true);
+
+    Chip_SCU_PinMuxSet (SEGMENT_P_PORT, SEGMENT_P_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | SEGMENT_P_FUNC);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_P_GPIO, SEGMENT_P_BIT, false);
+    Chip_GPIO_SetPinDIR (LPC_GPIO_PORT, SEGMENT_P_GPIO, SEGMENT_F_BIT, true);
+}
+
+void BuzzerInit(void){
+    Chip_SCU_PinMuxSet (BUZZER_PORT, BUZZER_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | BUZZER_FUNC);
+    board.buzzer = DigitalOutputCreate (BUZZER_GPIO, BUZZER_BIT);
+}
+
+void KeysInit (void){
+    Chip_SCU_PinMuxSet (KEY_ACCEPT_PORT, KEY_ACCEPT_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_ACCEPT_FUNC);
+    board.accept = DigitalInputCreate(KEY_ACCEPT_GPIO, KEY_ACCEPT_BIT, false);
+
+    Chip_SCU_PinMuxSet (KEY_CANCEL_PORT, KEY_CANCEL_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_CANCEL_FUNC);
+    board.cancel = DigitalInputCreate(KEY_CANCEL_GPIO, KEY_CANCEL_BIT, false);
+
+    Chip_SCU_PinMuxSet (KEY_F1_PORT, KEY_F1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_F1_FUNC);
+    board.set_time = DigitalInputCreate(KEY_F1_GPIO, KEY_F1_BIT, false);
+
+    Chip_SCU_PinMuxSet (KEY_F2_PORT, KEY_F2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_F2_FUNC);
+    board.set_alarm = DigitalInputCreate(KEY_F2_GPIO, KEY_F2_BIT, false);
+
+    Chip_SCU_PinMuxSet (KEY_F3_PORT, KEY_F3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_F3_FUNC);
+    board.decrement = DigitalInputCreate(KEY_F3_GPIO, KEY_F3_BIT, false);
+
+    Chip_SCU_PinMuxSet (KEY_F4_PORT, KEY_F4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | KEY_F4_FUNC);
+    board.increment = DigitalInputCreate(KEY_F4_GPIO, KEY_F4_BIT, false);
+}
+
+void ScreenTurnOff(void){
+    Chip_GPIO_ClearValue (LPC_GPIO_PORT, DIGITS_GPIO, DIGITS_MASK);
+    Chip_GPIO_ClearValue (LPC_GPIO_PORT, SEGMENTS_GPIO, SEGMENTS_MASK);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_P_GPIO, SEGMENT_P_BIT, false);
+}
+
+void SegmentsTurnOn(uint8_t segments){
+    Chip_GPIO_SetValue (LPC_GPIO_PORT, SEGMENTS_GPIO, (segments) & SEGMENTS_MASK);
+    Chip_GPIO_SetPinState (LPC_GPIO_PORT, SEGMENT_P_GPIO, SEGMENT_P_BIT, (segments & SEGMENT_P));
+
+void DigitTurnOn(uint8_t digit){
+    Chip_GPIO_SetValue (LPC_GPIO_PORT, DIGITS_GPIO, (1 << (digit)) & DIGITS_MASK);
+}
+
 board_puntero BoardCreate(void) {
-    /********* LED RGB **********/
-    Chip_SCU_PinMuxSet(LED_R_PORT, LED_R_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_R_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_R_GPIO, LED_R_BIT, true);
+    DigitsInit();
+    SegmentsInit();
+    BuzzerInit();
+    KeysInit();
 
-    Chip_SCU_PinMuxSet(LED_G_PORT, LED_G_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_G_FUNC);
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, LED_G_GPIO, LED_G_BIT, true);
-
-    Chip_SCU_PinMuxSet(LED_B_PORT, LED_B_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_B_FUNC);
-    board.led_azul = DigitalOutputCreate(LED_B_GPIO, LED_B_BIT);
-
-    /********** LEDs 1 2 y 3 ********/
-    Chip_SCU_PinMuxSet(LED_1_PORT, LED_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_1_FUNC);
-    board.led_rojo = DigitalOutputCreate(LED_1_GPIO, LED_1_BIT);
-
-    Chip_SCU_PinMuxSet(LED_2_PORT, LED_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_2_FUNC);
-    board.led_amarillo = DigitalOutputCreate(LED_2_GPIO, LED_2_BIT);
-
-    Chip_SCU_PinMuxSet(LED_3_PORT, LED_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_INACT | LED_3_FUNC);
-    board.led_verde = DigitalOutputCreate(LED_3_GPIO, LED_3_BIT);
-
-    /********* Teclas 1 2 3 y 4 *********/
-    Chip_SCU_PinMuxSet(TEC_1_PORT, TEC_1_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_1_FUNC);
-    board.Boton1 = DigitalInputCreate(TEC_1_GPIO, TEC_1_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_2_PORT, TEC_2_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_2_FUNC);
-    board.Boton2 = DigitalInputCreate(TEC_2_GPIO, TEC_2_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_3_PORT, TEC_3_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_3_FUNC);
-    board.Boton3 = DigitalInputCreate(TEC_3_GPIO, TEC_3_BIT, true);
-
-    Chip_SCU_PinMuxSet(TEC_4_PORT, TEC_4_PIN, SCU_MODE_INBUFF_EN | SCU_MODE_PULLUP | TEC_4_FUNC);
-    board.Boton4 = DigitalInputCreate(TEC_4_GPIO, TEC_4_BIT, true);
+    board.display = DisplayCreate(4, & (struct display_driver_estructura){
+        .ScreenTurnOff = ScreenTurnOff,
+        .SegmentsTurnOn = SegmentsTurnOn,
+        .DigitTurnOn = DigitTurnOn,
+    });
 
     return &board;
 }
